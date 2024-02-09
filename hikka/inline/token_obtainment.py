6c56aea5415922e6af4f3f766ee81951c20ee84a
nobianer <138612936+nobianer@users.noreply.github.com>
2024-02-09 17:18:38 +0200

    Update token_obtainment.py

--- hikka/inline/token_obtainment.py
+++ hikka/inline/token_obtainment.py
@@ -52,7 +52,7 @@ class TokenObtainment(InlineUnit):
                 username = f"@hikka_{uid}_bot"
 
             for msg in [
-                f"🌘 Soda Userbot of {self._name}"[:64],
+                f"🌘 Hikka Userbot of {self._name}"[:64],
                 username,
                 "/setuserpic",
                 username,
