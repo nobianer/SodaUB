5fcd67332e76ac946f495f0cc7c661e9da1a833b
nobianer <138612936+nobianer@users.noreply.github.com>
2024-02-09 16:08:15 +0200

    Update settings.py

--- hikka/modules/settings.py
+++ hikka/modules/settings.py
@@ -54,7 +54,7 @@ class CoreMod(loader.Module):
                 (
                     utils.get_platform_emoji()
                     if self._client.hikka_me.premium and CUSTOM_EMOJIS
-                    else "🌘 <b>Hikka userbot</b>"
+                    else "🌘 <b>Soda Userbot</b>"
                 ),
                 *version.__version__,
                 utils.get_commit_url(),
