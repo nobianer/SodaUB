e97ccad2c67c54941f856201acef301be9802be1
leiny <129380221+psleiny@users.noreply.github.com>
2024-02-09 19:36:10 +0200

    Update settings.py

--- hikka/modules/settings.py
+++ hikka/modules/settings.py
@@ -49,7 +49,7 @@ class CoreMod(loader.Module):
     async def sodacmd(self, message: Message):
         await utils.answer_file(
             message,
-            "https://imgur.com/nZXoaMq",
+            "https://raw.githubusercontent.com/psleiny/SodaLEiNY/main/Images/Nig.png",
             self.strings("hikka").format(
                 (
                     utils.get_platform_emoji()
