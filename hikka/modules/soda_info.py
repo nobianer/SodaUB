6174ac7a5b02e1f436ebef005aa4e8c918b78da8
nobianer <138612936+nobianer@users.noreply.github.com>
2024-03-22 02:56:34 +0200

    Update soda_info.py
    Невеликі зміни в інтерфейсі інфо-модулів

--- hikka/modules/soda_info.py
+++ hikka/modules/soda_info.py
@@ -20,7 +20,7 @@ class HikkaInfoMod(loader.Module):
             ),
             loader.ConfigValue(
                 "custom_button",
-                ["🌘 Support chat", "https://t.me/+4-YZDMXS3P5kYWQy"],
+                ["ℹ️ Support chat", "https://t.me/+4-YZDMXS3P5kYWQy"],
                 lambda: self.strings("_cfg_cst_btn"),
                 validator=loader.validators.Union(
                     loader.validators.Series(fixed_len=2),
@@ -72,7 +72,7 @@ class HikkaInfoMod(loader.Module):
 
         return (
             (
-                "<b>🌘 Soda Userbot</b>\n"
+                "<b>Soda UB</b>\n"
                 if "hikka" not in self.config["custom_message"].lower()
                 else ""
             )
@@ -108,7 +108,7 @@ class HikkaInfoMod(loader.Module):
                         (
                             utils.get_platform_emoji()
                             if self._client.hikka_me.premium and not inline
-                            else "🌘 Soda Userbot"
+                            else "Soda UB"
                         ),
                         "<emoji document_id=5373141891321699086>😎</emoji>",
                         "<emoji document_id=5469741319330996757>💫</emoji>",
